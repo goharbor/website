72d681d2684db3ab12249cbd89863354619fb253
lucperkins <lucperkins@gmail.com>
2020-02-26 12:14:49 -0800

    Add image modals
    Signed-off-by: lucperkins <lucperkins@gmail.com>

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,22 @@
-const navbarBurger = () => {
+function navbarBurger() {
   const burger = $(".navbar-burger"),
     menu = $(".navbar-menu");
 
-  burger.click(() => {
-
-
+  burger.click(function() {
     [burger, menu].forEach((el) => el.toggleClass('is-active'));
+  });
+}
+
+function imgModals() {
+  $(".img").click(function() {
+    const id = $(this).attr('id');
+    const modalId = `#modal-${id}`;
+
+    $(modalId).addClass('is-active');
+
+    $(`${modalId} .modal-close`).click(function() {
+      $(modalId).removeClass('is-active');
+    });
   });
 }
 
@@ -13,4 +24,5 @@ $(() => {
   console.log("Welcome to the Harbor docs");
 
   navbarBurger();
+  imgModals();
 });
