18db6e592e4e1174fca9ac3321bef5e1263ea9df
Maxime Alay-Eddine <maxime.alayeddine@gmail.com>
2023-06-08 10:21:09 +0200

    Add Cyberwatch in the Scanner Adapters list (#448)
    * Add Cyberwatch scanner logo
    
    Signed-off-by: Maxime Alay-Eddine <maxime.alayeddine@gmail.com>
    Signed-off-by: Maxime ALAY-EDDINE <maxime@cyberwatch.fr>
    
    * Fix feedback from Harbor team
    
    Signed-off-by: Maxime ALAY-EDDINE <maxime@cyberwatch.fr>
    
    ---------
    
    Signed-off-by: Maxime Alay-Eddine <maxime.alayeddine@gmail.com>
    Signed-off-by: Maxime ALAY-EDDINE <maxime@cyberwatch.fr>
    Co-authored-by: Maxime ALAY-EDDINE <maxime@cyberwatch.fr>
    Signed-off-by: OrlinVasilev <ovasilev@vmware.com>
    Signed-off-by: OrlinVasilev <ovasilev@vmware.com>

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -1,1 +1,13 @@
-orlix
+import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
+
+const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+  
+  const value = process.env.STASH_TOKEN;
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: `Value of STASH_TOKEN is ${value}.`}),
+  };  
+};
+
+export { handler };
